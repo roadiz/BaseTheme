4d398a95115c97e3847fb38354ddf6a5117f4753
Maxime Bérard <maxime@rezo-zero.com>
2015-01-05 14:53:49 +0100

    Added replace placeholder function

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -97,6 +97,41 @@ function getRandomInt(min, max) {
 
 
 /**
+ * Replace placeholder
+ * @return {[type]} [description]
+ */
+var replacePlaceholder = function() {
+
+    if(!Modernizr.input.placeholder){
+        
+        $('[placeholder]').focus(function() {
+          var input = $(this);
+          if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+          }
+        }).blur(function() {
+          var input = $(this);
+          if (input.val() === '' || input.val() == input.attr('placeholder')) {
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+          }
+        }).blur();
+        $('[placeholder]').parents('form').submit(function() {
+          $(this).find('[placeholder]').each(function() {
+            var input = $(this);
+            if (input.val() == input.attr('placeholder')) {
+              input.val('');
+            }
+          });
+        });
+
+    }
+
+};
+
+
+/**
  * Avoid `console` errors in browsers that lack a console.
  * @return {[type]} [description]
  */
