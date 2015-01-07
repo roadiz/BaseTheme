49c98e26139854cc7cf6e1e665771e7d6b0aefa0
Maxime Bérard <maxime@rezo-zero.com>
2015-01-07 15:32:06 +0100

    Remove double replacePlaceholder function

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -159,40 +159,6 @@ var replacePlaceholder = function() {
 
 
 /**
- * Replace placeholder for non-HTML5 compatible browsers.
- * @return {[type]} [description]
- */
-var replacePlaceholder = function() {
-
-    if(!Modernizr.input.placeholder){
-
-        $('[placeholder]').focus(function() {
-          var input = $(this);
-          if (input.val() == input.attr('placeholder')) {
-            input.val('');
-            input.removeClass('placeholder');
-          }
-        }).blur(function() {
-          var input = $(this);
-          if (input.val() === '' || input.val() == input.attr('placeholder')) {
-            input.addClass('placeholder');
-            input.val(input.attr('placeholder'));
-          }
-        }).blur();
-        $('[placeholder]').parents('form').submit(function() {
-          $(this).find('[placeholder]').each(function() {
-            var input = $(this);
-            if (input.val() == input.attr('placeholder')) {
-              input.val('');
-            }
-          });
-        });  
-    }
-
-};
-
-
-/**
  * JS / jQuery helper & plugins
  */
 
