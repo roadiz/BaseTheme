5e8c1f9bb8f563acab4b94410d602735449f4d5c
Ambroise Maupate <ambroise@rezo-zero.com>
2015-04-16 16:26:47 +0200

    Added some bootstrap.

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -3,12 +3,12 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.1d44ab4d.css',
+                '/vendor.2be3cc89.css',
                 '/style.6c2d744b.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.aa5b89de.min.js',
-                '/BaseTheme.a3186ace.min.js',
+                '/BaseTheme-vendor.7cd3647e.min.js',
+                '/BaseTheme.5d3a9d7a.min.js',
             ),
         ),
     )
