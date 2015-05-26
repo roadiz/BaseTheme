46e52ad4a37cb8141cc56fd8315b2aebd58fde56
Ambroise Maupate <ambroise@rezo-zero.com>
2015-05-26 16:42:49 +0200

    Updated grunt dependencies.

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -3,12 +3,12 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/vendor.2be3cc89.css',
-                '/style.121757da.css',
+                '/vendor.19d1cc83.css',
+                '/style.07f8828e.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.fa310a00.min.js',
-                '/BaseTheme.2e38251d.min.js',
+                '/BaseTheme-vendor.02a4c3b0.min.js',
+                '/BaseTheme.3a872549.min.js',
             ),
         ),
     )
