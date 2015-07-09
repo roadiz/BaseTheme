45810792a6bc9fcdaa97ad557e01a84affa6cc3e
Maxime Bérard <maxime@rezo-zero.com>
2015-07-09 17:21:24 +0200

    Grunt fail

---
+++ static/public/config/assets.config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'staticAssets' => array(
+        'global' => array(
+            'css' => array(
+                '/vendor.1e9c0f3b.css',
+                '/style.f468c577.css',
+            ),
+            'js' => array(
+                '/BaseTheme-vendor.91e8d871.min.js',
+                '/BaseTheme.316a6e79.min.js',
+            ),
+        ),
+    )
+);
