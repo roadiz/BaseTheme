008c9ac28ee13803a3e63a029a9a2fed33d7bec7
Ambroise Maupate <ambroise@rezo-zero.com>
2015-08-06 12:17:49 +0200

    Added better debug-panel styles.

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -4,11 +4,11 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1e9c0f3b.css',
-                '/style.e1da5867.css',
+                '/style.5415d392.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.3fdaa665.min.js',
-                '/BaseTheme.69538dbd.min.js',
+                '/BaseTheme-vendor.9f05e42c.min.js',
+                '/BaseTheme.b4d43b0c.min.js',
             ),
         ),
     )
