99c2bcff5253745fedb86a8f2de27650abe1e6e0
Maxime Bérard <maxime@rezo-zero.com>
2015-09-22 11:28:34 +0200

    Removed boostrap nav, added custom nav with open/close feature on xs screens

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -4,11 +4,11 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1e9c0f3b.css',
-                '/style.50cffed0.css',
+                '/style.94b0d273.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.8ff419f2.min.js',
-                '/BaseTheme.fad5ab24.min.js',
+                '/BaseTheme-vendor.f7dacd3b.min.js',
+                '/BaseTheme.aec1a01f.min.js',
             ),
         ),
     )
