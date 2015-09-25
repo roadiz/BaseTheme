5dddde0189b75f5d71cfb83bc384f5c20b436027
Maxime Bérard <maxime@rezo-zero.com>
2015-09-25 16:06:11 +0200

    Changed maintenance DOM, removed cookies choices JS styles

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -4,11 +4,11 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1e9c0f3b.css',
-                '/style.94b0d273.css',
+                '/style.2949daa7.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.f7dacd3b.min.js',
-                '/BaseTheme.aec1a01f.min.js',
+                '/BaseTheme-vendor.e3c11fbf.min.js',
+                '/BaseTheme.2b3bdc74.min.js',
             ),
         ),
     )
