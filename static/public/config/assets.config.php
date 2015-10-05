71af2e2fce37baadeedf12d6c7b5adc363ccd00d
Ambroise Maupate <ambroise@rezo-zero.com>
2015-10-05 09:48:32 +0200

    Fixed main container passing behing fixed nav

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -4,11 +4,11 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1e9c0f3b.css',
-                '/style.2949daa7.css',
+                '/style.86373ffa.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.e3c11fbf.min.js',
-                '/BaseTheme.2b3bdc74.min.js',
+                '/BaseTheme-vendor.b12bfc5a.min.js',
+                '/BaseTheme.68168166.min.js',
             ),
         ),
     )
