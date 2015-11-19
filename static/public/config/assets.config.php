2db796bbc7d2fb690ef239b856e780956c00de4f
Ambroise Maupate <ambroise@rezo-zero.com>
2015-11-19 16:53:09 +0100

    Fixed Readme and margin-top on main-container instead of top

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -4,11 +4,11 @@ return array(
         'global' => array(
             'css' => array(
                 '/vendor.1e9c0f3b.css',
-                '/style.86373ffa.css',
+                '/style.2228be2c.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.c8cf8e87.min.js',
-                '/BaseTheme.0a5e1629.min.js',
+                '/BaseTheme-vendor.d4745ddd.min.js',
+                '/BaseTheme.61517d74.min.js',
             ),
         ),
     )
