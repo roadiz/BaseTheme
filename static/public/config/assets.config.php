d08d05d487622beed45f29196797066604ea333d
Maxime Bérard <maxime@rezo-zero.com>
2015-12-21 12:10:08 +0100

    Removed styles for matrix icons, not used anymore (replaced by SVG icons)

--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -3,11 +3,11 @@ return array(
     'staticAssets' => array(
         'global' => array(
             'css' => array(
-                '/style.a2b7f17b.css',
+                '/style.5ee6d2ed.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.244ad74f.min.js',
-                '/BaseTheme.58432338.min.js',
+                '/BaseTheme-vendor.8d652b83.min.js',
+                '/BaseTheme.2913cade.min.js',
             ),
         ),
     )
