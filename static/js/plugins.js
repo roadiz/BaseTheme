d3b5cb33ff15493c8dc2b8f90226c5acdbeff17b
Maxime Bérard <maxime@rezo-zero.com>
2016-02-05 17:02:18 +0100

    Added disable/enable scroll methods.

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -293,6 +293,48 @@ var stripTrailingSlash = function(str) {
 
 
 /**
+ * Disable/enable scroll
+ */
+var keys = [37, 38, 39, 40, 33, 34, 35];
+// left: 37, up: 38, right: 39, down: 40, spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+
+function wheel(e) {
+    preventDefault(e);
+}
+
+function disableScroll() {
+    if (window.addEventListener) {
+        window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
+    document.onkeydown = keydown;
+}
+
+function enableScroll() {
+    if (window.removeEventListener) {
+        window.removeEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = document.onkeydown = null;
+}
+
+
+/**
  * Avoid `console` errors in browsers that lack a console.
  * @return {[type]} [description]
  */
--- static/public/config/assets.config.php
+++ static/public/config/assets.config.php
@@ -6,8 +6,8 @@ return array(
                 '/style.5ee6d2ed.css',
             ),
             'js' => array(
-                '/BaseTheme-vendor.39822e6b.min.js',
-                '/BaseTheme.ae5f935e.min.js',
+                '/BaseTheme-vendor.0686a93c.min.js',
+                '/BaseTheme.78472f18.min.js',
             ),
         ),
     )
