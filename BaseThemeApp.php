3212975e1f4f1044d04b8bb1fe6bc5c55b94af9b
Ambroise Maupate <ambroise@rezo-zero.com>
2016-08-22 10:12:00 +0200

    Bumped to 0.15.0

--- BaseThemeApp.php
+++ BaseThemeApp.php
@@ -37,7 +37,7 @@ use Themes\BaseTheme\Services;
  */
 class BaseThemeApp extends FrontendController
 {
-    const VERSION = '0.14.0';
+    const VERSION = '0.15.0';
 
     protected static $themeName = 'Base theme';
     protected static $themeAuthor = 'REZO ZERO';
