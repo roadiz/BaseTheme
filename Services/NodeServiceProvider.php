1561760a97ab23de3112a53f03ec78b3acbbddab
Ambroise Maupate <ambroise@rezo-zero.com>
2016-08-24 11:53:02 +0200

    Changed PHPDoc and copyrights.

--- Services/NodeServiceProvider.php
+++ Services/NodeServiceProvider.php
@@ -1,27 +1,11 @@
 <?php
 /**
- * Copyright © 2016, Ambroise Maupate
+ * Copyright (c) 2016. Rezo Zero
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
+ * BaseTheme
  *
  * @file NodeServiceProvider.php
- * @author Ambroise Maupate
+ * @author Ambroise Maupate <ambroise@rezo-zero.com>
  */
 namespace Themes\BaseTheme\Services;
 
@@ -38,6 +22,11 @@ class NodeServiceProvider implements ServiceProviderInterface
     protected $translation;
     protected $coreServices;
 
+    /**
+     * NodeServiceProvider constructor.
+     * @param Container $coreServices
+     * @param Translation|null $translation
+     */
     public function __construct(Container $coreServices, Translation $translation = null)
     {
         $this->coreServices = $coreServices;
@@ -46,6 +35,7 @@ class NodeServiceProvider implements ServiceProviderInterface
 
     /**
      * @param Container $container
+     * @return Container
      */
     public function register(Container $container)
     {
--- Services/NodeTypeServiceProvider.php
+++ Services/NodeTypeServiceProvider.php
@@ -1,27 +1,11 @@
 <?php
 /**
- * Copyright © 2016, Ambroise Maupate
+ * Copyright (c) 2016. Rezo Zero
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
+ * BaseTheme
  *
  * @file NodeTypeServiceProvider.php
- * @author Ambroise Maupate
+ * @author Ambroise Maupate <ambroise@rezo-zero.com>
  */
 namespace Themes\BaseTheme\Services;
 
@@ -35,7 +19,10 @@ class NodeTypeServiceProvider implements ServiceProviderInterface
      * @var NodeTypeApi
      */
     protected $api;
-
+    /**
+     * NodeTypeServiceProvider constructor.
+     * @param NodeTypeApi $api
+     */
     public function __construct(NodeTypeApi $api)
     {
         $this->api = $api;
@@ -43,6 +30,7 @@ class NodeTypeServiceProvider implements ServiceProviderInterface
 
     /**
      * @param Container $container
+     * @return Container
      */
     public function register(Container $container)
     {
