883e14abacbc646d289e179cb984c16dd778adfc
Maxime Bérard <maxime@rezo-zero.com>
2016-08-26 11:22:51 +0200

    👊 Added defaultblock & page to abstract all common methods

---
+++ static/src/blocks/default-block.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright REZO ZERO 2016
+ *
+ * @file default-block.js
+ * @copyright REZO ZERO 2016
+ * @author Maxime Bérard
+ */
+
+import $ from 'jquery';
+import {AbstractBlock} from 'abstract-block';
+
+/**
+ * Abstract class to do common actions on every blocks (like inView, etc.). Do not instanciate this class.
+ */
+export class DefaultBlock extends AbstractBlock
+{
+    init() {
+        super.init();
+    }
+
+    // initEvents(){
+    //     super.initEvents();
+    // }
+
+    // destroyEvents(){
+    //     super.destroyEvents();
+    // }
+}
