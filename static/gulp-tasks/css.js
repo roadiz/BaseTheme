0c7cdc6b853941e56e5c1de5cf443425d37f94bc
Ambroise Maupate <ambroise@rezo-zero.com>
2016-10-10 14:47:22 +0200

    Update postCSS filters.

--- static/gulp-tasks/css.js
+++ static/gulp-tasks/css.js
@@ -1,15 +1,15 @@
 require('es6-promise').polyfill();
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
-var mqpacker = require('css-mqpacker');
-var csswring = require('csswring');
 
 gulp.task('css', ['less-vendor', 'less'], function () {
     var processors = [
-        autoprefixer({browsers: ['last 3 version', 'ie 9']}),
-        mqpacker,
-        csswring
+        require('postcss-fixes')(),
+        require('postcss-filter-gradient')(),
+        require('postcss-reduce-transforms')(),
+        require('autoprefixer')({browsers: ['last 4 version', 'ie 11', 'ie 10', 'ie 9']}),
+        require('css-mqpacker')(),
+        require('cssnano')
     ];
     return gulp.src('./css/*.css')
         .pipe(postcss(processors))
