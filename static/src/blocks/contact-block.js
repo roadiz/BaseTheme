1653e125370c9099ef2553886404494b5805272e
Ambroise Maupate <ambroise@rezo-zero.com>
2017-01-27 22:13:12 +0100

    Improved contact-block.js example.

--- static/src/blocks/contact-block.js
+++ static/src/blocks/contact-block.js
@@ -5,10 +5,10 @@
  * @copyright REZO ZERO 2016
  * @author Maxime Bérard
  */
-import $                     from "jquery";
-import log                   from "loglevel/dist/loglevel";
-import DefaultBlock          from "./default-block";
-import {TweenLite, EasePack} from "gsap";
+import $ from "jquery";
+import {TweenLite} from "gsap";
+import log from "loglevel/dist/loglevel";
+import DefaultBlock from "./default-block";
 
 export default class ContactBlock extends DefaultBlock {
 
@@ -18,63 +18,64 @@ export default class ContactBlock extends DefaultBlock {
         this.$form = this.$cont.find('form');
         this.$formBtn = this.$form.find('button');
         this.$formMessage = this.$cont.find('.form-message');
+        this.bindOnSubmit = this.formSubmit.bind(this);
     }
 
     initEvents() {
         super.initEvents();
 
-        /*
-         * Un-comment to handle contact form with AJAX
-         */
-        //if(this.$form.length) this.$form.on('submit', $.proxy(this.formSubmit, this));
+        if(this.$form.length) this.$form.on('submit', this.bindOnSubmit);
     }
 
     destroyEvents() {
         super.destroyEvents();
 
-        /*
-         * Un-comment to handle contact form with AJAX
-         */
-        //if(this.$form.length) this.$form.off('submit', $.proxy(this.formSubmit, this));
+        if(this.$form.length) this.$form.off('submit', this.bindOnSubmit);
     }
 
     formSubmit(e) {
-        TweenLite.to(this.$formMessage, 0.4, {height:0});
-
-        $.ajax({
-            url: e.currentTarget.action,
-            data : this.$form.serialize(),
-            type: 'post',
-            dataType: 'json',
-            success: (data) => {
-                log.debug('SUCCESS');
-                log.debug(data.status);
-                if (data.status != 'success') {
-                    this.$formMessage[0].className = 'form-message form-message-'+data.status;
-                    this.$formMessage[0].innerHTML = '<span>'+data.message+'</span>';
-
-                } else {
-                    this.$formMessage[0].className = 'form-message form-message-hidden form-message-'+data.status;
-                    this.$formMessage[0].innerHTML = '<span>'+data.message+'</span>';
-                }
-
-                let height = this.$formMessage.find('span').actual('outerHeight');
-                TweenLite.to(this.$formMessage, 0.6, {height:height, delay:0.2});
-
-            },
-            error: (data) => {
-                log.debug('ERROR');
-                data = data.responseJSON;
-                log.debug(data);
-                this.$formMessage[0].className = 'form-message form-message-hidden form-message-error form-message-'+data.status;
-                this.$formMessage[0].innerHTML = '<span>'+data.errors+'</span>';
-
-                let height = this.$formMessage.find('span').actual('outerHeight');
-                TweenLite.to(this.$formMessage, 0.6, {height:height, delay:0.2});
-            }
-        });
-
         e.preventDefault();
+
+        if (this.timeout) {
+            clearTimeout(this.timeout)
+        }
+        this.timeout = setTimeout(() => {
+            TweenLite.to(this.$formMessage, 0.6, {height:0, paddingTop:0, paddingBottom: 0});
+            const data = new FormData(this.$form[0]);
+
+            $.ajax({
+                url: e.currentTarget.action,
+                data : data,
+                processData : false,
+                cache : false,
+                contentType : false,
+                type: 'post',
+                dataType: 'json',
+                success: (data) => {
+                    log.debug(data.status);
+                    if (data.status != 'success') {
+                        this.$formMessage[0].className = 'form-message alert alert-'+data.status;
+                        this.$formMessage[0].innerHTML = '<span>'+data.message+'</span>';
+                    } else {
+                        this.$formMessage[0].className = 'form-message form-message-hidden alert alert-'+data.status;
+                        this.$formMessage[0].innerHTML = '<span>'+data.message+'</span>';
+                    }
+
+                    TweenLite.set(this.$formMessage, {height:48, paddingTop:15, paddingBottom: 15});
+                    TweenLite.from(this.$formMessage, 0.6, {height:0, paddingTop:0, paddingBottom: 0});
+                },
+                error: (data) => {
+                    data = data.responseJSON;
+                    log.debug(data);
+                    this.$formMessage[0].className = 'form-message form-message-hidden alert-danger alert-'+data.status;
+                    this.$formMessage[0].innerHTML = '<span>'+data.errors+'</span>';
+
+                    TweenLite.set(this.$formMessage, {height:48, paddingTop:15, paddingBottom: 15});
+                    TweenLite.from(this.$formMessage, 0.6, {height:0, paddingTop:0, paddingBottom: 0});
+                }
+            });
+        }, 400);
+
+        return false;
     }
 }
-
