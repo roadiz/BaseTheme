1b83e4a40d4cf322e23441cf98d4d9076bad76c2
Ambroise Maupate <ambroise@rezo-zero.com>
2018-04-25 11:05:55 +0200

    Improve JS and added Google Tag Manager and service for legals page.

--- BaseThemeApp.php
+++ BaseThemeApp.php
@@ -142,6 +142,8 @@ class BaseThemeApp extends FrontendController
         $this->assignation['head']['googleClientId'] = $this->get('settingsBag')->get('google_client_id');
         $this->assignation['head']['twitterAccount'] = $this->get('settingsBag')->get('twitter_account');
         $this->assignation['head']['mapsStyle'] = $this->get('settingsBag')->get('maps_style');
+        $this->assignation['head']['googleTagManagerId'] = $this->get('settingsBag')->get('google_tag_manager_id');
+
         $this->assignation['head']['themeName'] = static::$themeName;
         $this->assignation['head']['themeVersion'] = static::VERSION;
 
