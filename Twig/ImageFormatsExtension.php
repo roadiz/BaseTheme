f5b0a669f65a799eefd1c81ca9481fceef677d46
Ambroise Maupate <ambroise@rezo-zero.com>
2018-05-15 18:07:28 +0200

    Use Twig extension for image formats.

---
+++ Twig/ImageFormatsExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Themes\BaseTheme\Twig;
+
+
+class ImageFormatsExtension extends \Twig_Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function getGlobals()
+    {
+        return [
+            'imageFormats' => [
+                'headerImage' => [
+                    'fit' => '1920x300',
+                    'quality' => 85,
+                    'progressive' => true,
+                    'class' => 'img-fluid',
+                ],
+                'columnedImage' => [
+                    'width' => 720,
+                    'progressive' => true,
+                    'class' => 'img-fluid',
+                ],
+                'thumbnail' => [
+                    'fit' => '600x338',
+                    'controls' => true,
+                    'embed' => true,
+                    'progressive' => true
+                ],
+                'shareImage' => [
+                    'fit' => '1200x630',
+                    'absolute' => true,
+                    'progressive' => true,
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName()
+    {
+        return 'BaseThemeImageFormats';
+    }
+}
