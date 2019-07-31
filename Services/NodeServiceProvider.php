394ce2271efb35fdfee1f9be7b69408efac5f18c
Ambroise Maupate <ambroise@rezo-zero.com>
2019-07-31 17:16:38 +0200

    Use nodeSource for main-menu

--- Services/NodeServiceProvider.php
+++ Services/NodeServiceProvider.php
@@ -43,11 +43,13 @@ class NodeServiceProvider implements ServiceProviderInterface
          * Get main-menu root node
          * whatever its translation.
          */
-        $container['nodeMenu'] = function () {
-            return $this->coreServices['nodeApi']
-                        ->getOneBy([
-                            'nodeName' => 'main-menu',
-                        ]);
+        $container['nodeSourceMenu'] = function () {
+            return $this->coreServices['nodeSourceApi']
+                ->getOneBy([
+                    'node.nodeName' => 'main-menu',
+                    'node.nodeType' => $this->coreServices['nodeTypesBag']->get('Neutral'),
+                    'translation' => $this->translation,
+                ]);
         };
 
         /*
@@ -55,11 +57,11 @@ class NodeServiceProvider implements ServiceProviderInterface
          * This is nodeSources !
          */
         $container['navigation'] = function ($c) {
-            if ($c['nodeMenu'] !== null) {
+            if ($c['nodeSourceMenu'] !== null) {
                 return $this->coreServices['nodeSourceApi']
                     ->getBy(
                         [
-                            'node.parent' => $c['nodeMenu'],
+                            'node.parent' => $c['nodeSourceMenu']->getNode(),
                             'node.visible' => true,
                             'translation' => $this->translation,
                         ],
