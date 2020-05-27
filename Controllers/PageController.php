7a8a5ec9f5dfe620c82e5805495882d761b992aa
Ambroise Maupate <ambroise@rezo-zero.com>
2020-05-27 03:05:26 +0200

    Require v1.4

--- Controllers/PageController.php
+++ Controllers/PageController.php
@@ -3,11 +3,6 @@ declare(strict_types=1);
 
 namespace Themes\BaseTheme\Controllers;
 
-use RZ\Roadiz\Core\Entities\Node;
-use RZ\Roadiz\Core\Entities\Translation;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 /**
  * Class PageController
  * @package Themes\BaseTheme\Controllers
