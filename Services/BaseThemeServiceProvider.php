73166dce20bfaf7374eea8e5765293f5b666247c
Ambroise Maupate <ambroise@rezo-zero.com>
2020-07-16 11:00:10 +0200

    Separate orejime translation files

--- Services/BaseThemeServiceProvider.php
+++ Services/BaseThemeServiceProvider.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Translation\Translator;
 use Themes\BaseTheme\Event\LinkPathGeneratingEventListener;
 use Themes\BaseTheme\Twig\ImageFormatsExtension;
 
@@ -14,6 +15,20 @@ class BaseThemeServiceProvider implements ServiceProviderInterface
 {
     public function register(Container $container)
     {
+        $container->extend('translator', function (Translator $translator) {
+            $translator->addResource(
+                'xlf',
+                dirname(__DIR__) . '/Resources/translations/orejime.en.xlf',
+                'en'
+            );
+            $translator->addResource(
+                'xlf',
+                dirname(__DIR__) . '/Resources/translations/orejime.fr.xlf',
+                'fr'
+            );
+            return $translator;
+        });
+
         $container->extend('dispatcher', function (EventDispatcherInterface $dispatcher) {
             /*
              * Path generating subscriber to transform NSLink URL in their linked node Path
